72e1efeddac75954a7e7e00979f51a3dc1dc4c2e
Mairarairez <maira_ramirez2@hotmail.com>
2023-07-05 09:13:37 -0300

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ContactFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
